348e7b81fb3771563a6756e533cbd91bdf8c54ee
Silvan Schmidt <sil.schmidt@proton.me>
2023-12-17 13:37:07 +0100

    Backup

--- crates/rnote-ui/src/settingswindow.rs
+++ crates/rnote-ui/src/settingswindow.rs
@@ -1,14 +1,86 @@
+use std::cell::RefCell;
+
 use adw::prelude::*;
 use adw::subclass::prelude::*;
 
-use gtk4::{glib, CompositeTemplate};
+use gtk4::{
+    glib, Adjustment, Button, ColorDialogButton, CompositeTemplate, MenuButton, ScrolledWindow,
+    ToggleButton,
+};
+
+use crate::{RnAppWindow, RnCanvasWrapper, RnIconPicker, RnUnitEntry};
+use rnote_engine::document::Format;
 
 mod imp {
     use super::*;
 
     #[derive(Default, Debug, CompositeTemplate)]
     #[template(resource = "/com/github/flxzt/rnote/ui/settingswindow.ui")]
-    pub(crate) struct RnSettingsWindow {}
+    pub(crate) struct RnSettingsWindow {
+        pub(crate) temporary_format: RefCell<Format>,
+        pub(crate) app_restart_toast_singleton: RefCell<Option<adw::Toast>>,
+
+        #[template_child]
+        pub(crate) settings_scroller: TemplateChild<ScrolledWindow>,
+        #[template_child]
+        pub(crate) general_autosave_row: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub(crate) general_autosave_interval_secs_row: TemplateChild<adw::SpinRow>,
+        #[template_child]
+        pub(crate) general_show_scrollbars_row: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub(crate) general_inertial_scrolling_row: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub(crate) general_regular_cursor_picker: TemplateChild<RnIconPicker>,
+        #[template_child]
+        pub(crate) general_regular_cursor_picker_menubutton: TemplateChild<MenuButton>,
+        #[template_child]
+        pub(crate) general_show_drawing_cursor_row: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub(crate) general_drawing_cursor_picker_row: TemplateChild<adw::ActionRow>,
+        #[template_child]
+        pub(crate) general_drawing_cursor_picker: TemplateChild<RnIconPicker>,
+        #[template_child]
+        pub(crate) general_drawing_cursor_picker_menubutton: TemplateChild<MenuButton>,
+        #[template_child]
+        pub(crate) format_predefined_formats_row: TemplateChild<adw::ComboRow>,
+        #[template_child]
+        pub(crate) format_orientation_row: TemplateChild<adw::ActionRow>,
+        #[template_child]
+        pub(crate) format_orientation_portrait_toggle: TemplateChild<ToggleButton>,
+        #[template_child]
+        pub(crate) format_orientation_landscape_toggle: TemplateChild<ToggleButton>,
+        #[template_child]
+        pub(crate) format_width_row: TemplateChild<adw::ActionRow>,
+        #[template_child]
+        pub(crate) format_width_unitentry: TemplateChild<RnUnitEntry>,
+        #[template_child]
+        pub(crate) format_height_row: TemplateChild<adw::ActionRow>,
+        #[template_child]
+        pub(crate) format_height_unitentry: TemplateChild<RnUnitEntry>,
+        #[template_child]
+        pub(crate) format_dpi_row: TemplateChild<adw::SpinRow>,
+        #[template_child]
+        pub(crate) format_dpi_adj: TemplateChild<Adjustment>,
+        #[template_child]
+        pub(crate) format_revert_button: TemplateChild<Button>,
+        #[template_child]
+        pub(crate) format_apply_button: TemplateChild<Button>,
+        #[template_child]
+        pub(crate) doc_format_border_color_button: TemplateChild<ColorDialogButton>,
+        #[template_child]
+        pub(crate) doc_background_color_button: TemplateChild<ColorDialogButton>,
+        #[template_child]
+        pub(crate) doc_background_patterns_row: TemplateChild<adw::ComboRow>,
+        #[template_child]
+        pub(crate) doc_background_pattern_color_button: TemplateChild<ColorDialogButton>,
+        #[template_child]
+        pub(crate) doc_background_pattern_width_unitentry: TemplateChild<RnUnitEntry>,
+        #[template_child]
+        pub(crate) doc_background_pattern_height_unitentry: TemplateChild<RnUnitEntry>,
+        #[template_child]
+        pub(crate) background_pattern_invert_color_button: TemplateChild<Button>,
+    }
 
     #[glib::object_subclass]
     impl ObjectSubclass for RnSettingsWindow {
